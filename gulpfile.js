acc0ba9996a9e4c4c9be0f34114590b5752cad36
Kathy Zadrozny <kzadrozny@atlantis.lib.uchicago.edu>
2018-01-19 15:24:59 -0600

    Dashboard and Article Request Form

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+
+//Compile Sass to css
+gulp.task('compileSass', function() {
+  return gulp.src(['css/**/*.scss'])
+    .pipe(maps.init()) // Sass source map 1/2
+    .pipe(sass())
+    .pipe(maps.write('./')) // Sass source map 2/2
+    .pipe(gulp.dest('css'))
+  .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Watch Files
+gulp.task('watchFiles', ['browserSync', 'compileSass'], function() {
+  gulp.watch('css/**/*.scss', ['compileSass'], browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+});
+
+// Load and refresh on local
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task("build", ['compileSass', 'watchFiles']);
+
+gulp.task("default", ['build']);
